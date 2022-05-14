c4a4066311d7e0a3643742ec8869a4016a34d8d5
dmitry-weirdo <dmitry.weirdo@gmail.com>
2022-05-14 22:27:53 +0200

    feat: Migrate WikiBotGoogleSheet to a data Kotlin class.

--- src/main/java/com/dv/telegram/google/WikiBotGoogleSheet.kt
+++ src/main/java/com/dv/telegram/google/WikiBotGoogleSheet.kt
@@ -1,13 +1,8 @@
-package com.dv.telegram.google;
+package com.dv.telegram.google
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class WikiBotGoogleSheet { // only contains strings from the Google Sheet, not semantically parsed
-    private SheetData wikiPagesSheet;
-    private SheetData cityChatsSheet;
-    private SheetData countryChatsSheet;
-    private SheetData commandsSheet;
-}
+data class WikiBotGoogleSheet( // only contains strings from the Google Sheet, not semantically parsed
+    val wikiPagesSheet: SheetData,
+    val cityChatsSheet: SheetData,
+    val countryChatsSheet: SheetData,
+    val commandsSheet: SheetData
+)
