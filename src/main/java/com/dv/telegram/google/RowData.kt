811db888de2143f11b84879e70904808e4c508a3
dmitry-weirdo <dmitry.weirdo@gmail.com>
2022-05-14 21:24:48 +0200

    feat: Migrated RowData to Kotlin. Added a test. Code significantly simplified.

--- src/main/java/com/dv/telegram/google/RowData.kt
+++ src/main/java/com/dv/telegram/google/RowData.kt
@@ -1,28 +1,11 @@
-package com.dv.telegram.google;
+package com.dv.telegram.google
 
-import lombok.Data;
+class RowData {
+    val cells: MutableList<String> = mutableListOf()
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Data
-public class RowData {
-
-    private List<String> cells;
-
-    public RowData() {
-        this.cells = new ArrayList<>();
+    fun addCell(cell: String) {
+        cells.add(cell)
     }
 
-    public void addCell(String cell) {
-        cells.add(cell);
-    }
-
-    public String getCellOrBlank(int index) {
-        if ((0 <= index) && (index < cells.size())) {
-            return cells.get(index);
-        }
-
-        return "";
-    }
+    fun getCellOrBlank(index: Int): String = cells.getOrElse(index) { "" }
 }
