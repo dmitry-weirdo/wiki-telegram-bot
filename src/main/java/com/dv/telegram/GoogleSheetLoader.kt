8716bd048134c21a60d4b4e4024018c6ebb31397
dmitry-weirdo <dmitry.weirdo@gmail.com>
2022-05-14 23:00:20 +0200

    feat: Migrate GoogleSheetLoader to a Kotlin object.

--- src/main/java/com/dv/telegram/GoogleSheetLoader.kt
+++ src/main/java/com/dv/telegram/GoogleSheetLoader.kt
@@ -1,49 +1,42 @@
-package com.dv.telegram;
+package com.dv.telegram
 
-import com.dv.telegram.data.CityChatsParser;
-import com.dv.telegram.data.CommandsParser;
-import com.dv.telegram.data.CountryChatsParser;
-import com.dv.telegram.data.WikiPagesParser;
-import com.dv.telegram.exception.CommandException;
-import com.dv.telegram.google.GoogleSheetReader;
-import com.dv.telegram.google.WikiBotGoogleSheet;
-import lombok.extern.log4j.Log4j2;
+import com.dv.telegram.data.CityChatsParser
+import com.dv.telegram.data.CommandsParser
+import com.dv.telegram.data.CountryChatsParser
+import com.dv.telegram.data.WikiPagesParser
+import com.dv.telegram.exception.CommandException
+import com.dv.telegram.google.GoogleSheetReader
+import org.apache.logging.log4j.kotlin.Logging
 
-import java.util.List;
+object GoogleSheetLoader : Logging {
 
-@Log4j2
-public final class GoogleSheetLoader {
-
-    private GoogleSheetLoader() {
-    }
-
-    public static GoogleSheetBotData readGoogleSheet(WikiBotConfig config) {
+    @JvmStatic
+    fun readGoogleSheet(config: WikiBotConfig): GoogleSheetBotData {
         try {
-            log.info("Loading bot data from the Google Sheet...");
-            GoogleSheetBotData botData = reloadGoogleSheetUnsafe(config);
-            log.info("Bot data successfully reloaded from the Google Sheet.");
+            logger.info("Loading bot data from the Google Sheet...")
+            val botData = reloadGoogleSheetUnsafe(config)
+            logger.info("Bot data successfully reloaded from the Google Sheet.")
 
-            return botData;
+            return botData
         }
-        catch (Exception e) {
-            log.error("Error when loading bot data from the Google Sheet", e);
-            throw new CommandException("При загрузке данных из Google Sheet произошла ошибка.");
+        catch (e: Exception) {
+            logger.error("Error when loading bot data from the Google Sheet", e)
+            throw CommandException("При загрузке данных из Google Sheet произошла ошибка.")
         }
     }
 
-    private static GoogleSheetBotData reloadGoogleSheetUnsafe(WikiBotConfig config) {
-        WikiBotGoogleSheet wikiBotGoogleSheet = GoogleSheetReader.readGoogleSheetSafe(config);
+    private fun reloadGoogleSheetUnsafe(config: WikiBotConfig): GoogleSheetBotData {
+        val wikiBotGoogleSheet = GoogleSheetReader.readGoogleSheetSafe(config)
+        val wikiPages = WikiPagesParser.parseWikiPages(wikiBotGoogleSheet)
+        val cityChatsData = CityChatsParser.parseCityChats(wikiBotGoogleSheet)
+        val countryChatsData = CountryChatsParser.parseCountryChats(wikiBotGoogleSheet)
+        val botCommands = CommandsParser.parseWikiBotCommands(wikiBotGoogleSheet)
 
-        List<WikiPageData> wikiPages = WikiPagesParser.parseWikiPages(wikiBotGoogleSheet);
-        List<CityChatData> cityChatsData = CityChatsParser.parseCityChats(wikiBotGoogleSheet);
-        List<CountryChatData> countryChatsData = CountryChatsParser.parseCountryChats(wikiBotGoogleSheet);
-        List<WikiBotCommandData> botCommands = CommandsParser.parseWikiBotCommands(wikiBotGoogleSheet);
-
-        return new GoogleSheetBotData(
+        return GoogleSheetBotData(
             wikiPages,
             cityChatsData,
             countryChatsData,
             botCommands
-        );
+        )
     }
 }
