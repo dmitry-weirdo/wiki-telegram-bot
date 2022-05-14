56b4b992815121be8d504f4b81fb8b603c48d446
dmitry-weirdo <dmitry.weirdo@gmail.com>
2022-05-14 23:40:18 +0200

    feat: Migrate BotTriggerMode to a Kotlin class.

--- src/main/java/com/dv/telegram/config/BotSetting.kt
+++ src/main/java/com/dv/telegram/config/BotSetting.kt
@@ -2,8 +2,8 @@ package com.dv.telegram.config
 
 sealed interface BotSetting<T> {
     val name: String // English name, no spaces
-
     val description: String // description, in Russian
+
     fun getValue(): T
     fun setValue(value: String)
 
--- src/main/java/com/dv/telegram/config/BotTriggerMode.kt
+++ src/main/java/com/dv/telegram/config/BotTriggerMode.kt
@@ -1,50 +1,40 @@
-package com.dv.telegram.config;
+package com.dv.telegram.config
 
-import lombok.Data;
+class BotTriggerMode : BotSetting<BotTriggerMode.Mode> {
 
-@Data
-public class BotTriggerMode implements BotSetting<BotTriggerMode.Mode> {
+    companion object {
+        // class simpleName cannot be const, see https://stackoverflow.com/questions/37182900/static-const-in-kotlin-from-java-class-name
+        const val NAME: String = "BotTriggerMode"
+    }
 
-    public static final String NAME = BotTriggerMode.class.getSimpleName();
-
-    public enum Mode {
-        ANY_SUBSTRING, // любая подстрока с именем бота, в том числе, внутри другого слова
-        STRING_START, // только имя бота вначале сообщения
+    enum class Mode {
+        ANY_SUBSTRING,  // любая подстрока с именем бота, в том числе, внутри другого слова
+        STRING_START,  // только имя бота вначале сообщения
         FULL_WORD // только имя бота как отдельное слово
     }
 
-    private Mode mode;
+    private var mode: Mode = Mode.FULL_WORD
 
-    @Override
-    public String getName() {
-        return NAME;
-    }
+    override val name = NAME
 
-    @Override
-    public String getDescription() {
-        return """
-            *ANY_SUBSTRING* — Вызов бота триггерит любая подстрока с его именем. Например, сообщение «клубника» триггернёт бота «Ника».
+    override val description = """
+        *${Mode.ANY_SUBSTRING.name}* — Вызов бота триггерит любая подстрока с его именем. Например, сообщение «клубника» триггернёт бота «Ника».
+    
+        *${Mode.STRING_START.name}* — Вызов бота триггерит только сообщение, которое начинается с имени бота.
+    
+        *${Mode.FULL_WORD.name}* — Вызов бота триггерит только сообщение, в котором имя бота указано как отдельное слово.
+        
+        Во всех случаях имя бота можно указывать в любом регистре.
+        """.trimIndent()
 
-            *STRING_START* — Вызов бота триггерит только сообщение, которое начинается с имени бота.
+    override fun getValue(): Mode = mode
 
-            *FULL_WORD* — Вызов бота триггерит только сообщение, в котором имя бота указано как отдельное слово.
-            
-            Во всех случаях имя бота можно указывать в любом регистре.
-            """;
-    }
-
-    @Override
-    public Mode getValue() {
-        return mode;
-    }
-
-    @Override
-    public void setValue(String value) {
+    override fun setValue(value: String) {
         try {
-            mode = Mode.valueOf(value);
+            mode = Mode.valueOf(value)
         }
-        catch (IllegalArgumentException e) {
-            throw new SettingValidationException(String.format("Incorrect Mode value: \"%s\".", value));
+        catch (e: IllegalArgumentException) {
+            throw SettingValidationException("Incorrect Mode value: \"$value\".")
         }
     }
 }
