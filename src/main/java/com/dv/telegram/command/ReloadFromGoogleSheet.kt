2d915b0512b3dc90661f87b9bef60abbffc8093a
dmitry-weirdo <dmitry.weirdo@gmail.com>
2022-06-06 18:58:25 +0200

    refactor: Migrated ReloadFromGoogleSheet command to Kotlin.

--- src/main/java/com/dv/telegram/command/ReloadFromGoogleSheet.kt
+++ src/main/java/com/dv/telegram/command/ReloadFromGoogleSheet.kt
@@ -1,35 +1,21 @@
-package com.dv.telegram.command;
+package com.dv.telegram.command
 
-import com.dv.telegram.WikiBot;
+import com.dv.telegram.WikiBot
 
-public class ReloadFromGoogleSheet extends BasicBotCommand {
+class ReloadFromGoogleSheet : BasicBotCommand() {
+    override val name: String = javaClass.simpleName
 
-    @Override
-    public String getName() {
-        return ReloadFromGoogleSheet.class.getSimpleName();
-    }
+    override fun getDescription(bot: WikiBot) =
+        "`${bot.botName} $commandText` — перезагрузить конфигурацию бота из Google Sheet."
 
-    @Override
-    public String getDescription(WikiBot bot) {
-        return String.format(
-            "`%s %s` — перезагрузить конфигурацию бота из Google Sheet.",
-            bot.getBotName(),
-            getCommandText()
-        );
-    }
+    override val defaultCommandName = "/gs-reload-5150" // "secret" name to not be guessed by the user
 
-    @Override
-    public String getDefaultCommandName() {
-        return "/gs-reload-5150"; // "secret" name to not be guessed by the user
-    }
+    override fun getResponse(text: String, bot: WikiBot): String {
+        val reloadedSuccessful = bot.reloadBotDataFromGoogleSheet()
 
-    @Override
-    public String getResponse(String text, WikiBot bot) {
-        boolean reloadedSuccessful = bot.reloadBotDataFromGoogleSheet();
-
-        return reloadedSuccessful
-            ? "Данные бота успешно загружены из Google Sheet."
-            : "При загрузке данных из Google Sheet произошла ошибка."
-        ;
+        return if (reloadedSuccessful)
+            "Данные бота успешно загружены из Google Sheet."
+        else
+            "При загрузке данных из Google Sheet произошла ошибка."
     }
 }
