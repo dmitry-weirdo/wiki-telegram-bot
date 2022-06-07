6fb45c3379486dd726e6eaaf06caeca62f351d24
dmitry-weirdo <dmitry.weirdo@gmail.com>
2022-06-07 18:43:16 +0200

    refactor: Migrated NotionCityChat (trivial data class) to Kotlin.

--- src/main/java/com/dv/telegram/notion/NotionCityChat.kt
+++ src/main/java/com/dv/telegram/notion/NotionCityChat.kt
@@ -1,13 +1,6 @@
-package com.dv.telegram.notion;
+package com.dv.telegram.notion
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class NotionCityChat {
-    private String url;
-    private String name;
-}
+data class NotionCityChat (
+    val url: String?,
+    val name: String?
+)
