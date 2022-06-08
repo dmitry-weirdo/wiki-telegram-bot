2719fbd92036c39384e3570a8d8045d36ba07c1e
dmitry-weirdo <dmitry.weirdo@gmail.com>
2022-06-08 02:01:11 +0200

    refactor: Migrated WikiBotUtils class to Kotlin object.

--- src/main/java/com/dv/telegram/util/WikiBotUtils.kt
+++ src/main/java/com/dv/telegram/util/WikiBotUtils.kt
@@ -1,36 +1,27 @@
-package com.dv.telegram.util;
+package com.dv.telegram.util
 
-import com.dv.telegram.WikiBotConfig;
-import com.dv.telegram.WikiBotConfigs;
-import org.apache.commons.lang3.StringUtils;
+import com.dv.telegram.WikiBotConfig
+import com.dv.telegram.WikiBotConfigs
 
-public final class WikiBotUtils {
+object WikiBotUtils {
+    private const val WIKI_BOT_CONFIG_FILE_PATH_ENV_NAME = "WIKI_BOT_CONFIG_FILE_PATH"
 
-    public static final String WIKI_BOT_CONFIG_FILE_PATH_ENV_NAME = "WIKI_BOT_CONFIG_FILE_PATH";
-
-    private WikiBotUtils() {
+    fun readConfig(): WikiBotConfig {
+        return JacksonUtils.parseConfig(configFilePath)
     }
 
-    public static WikiBotConfig readConfig() {
-        String configFilePath = getConfigFilePath();
-        return JacksonUtils.parseConfig(configFilePath);
+    fun readConfigs(): WikiBotConfigs {
+        return JacksonUtils.parseConfigs(configFilePath)
     }
 
-    public static WikiBotConfigs readConfigs() {
-        String configFilePath = getConfigFilePath();
-        return JacksonUtils.parseConfigs(configFilePath);
-    }
+    private val configFilePath: String
+        get() = getEnvVariable(WIKI_BOT_CONFIG_FILE_PATH_ENV_NAME)
 
-    public static String getConfigFilePath() {
-        return getEnvVariable(WIKI_BOT_CONFIG_FILE_PATH_ENV_NAME);
-    }
+    fun getEnvVariable(name: String): String {
+        val value = System.getenv(name)
 
-    public static String getEnvVariable(String name) {
-        String value = System.getenv(name);
-        if (StringUtils.isBlank(value)) {
-            throw new IllegalStateException(String.format("Environment variable %s is not set.", name));
-        }
+        check(!value.isNullOrBlank()) { "Environment variable $name is not set." } // check will throw ISE if check not passed
 
-        return value;
+        return value
     }
 }
