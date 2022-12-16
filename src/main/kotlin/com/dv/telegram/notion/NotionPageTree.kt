47dbfd9ffa35cb909c282f41be5d61f3086634f1
dmitry-weirdo <dmitry.weirdo@gmail.com>
2022-12-16 22:02:29 +0100

    feat: Build a readable blocks tree as a single string to translate.

--- src/main/kotlin/com/dv/telegram/notion/NotionPageTree.kt
+++ src/main/kotlin/com/dv/telegram/notion/NotionPageTree.kt
@@ -18,12 +18,17 @@ object NotionPageTree : Logging {
         val notionToken = WikiBotUtils.getEnvVariable(NOTION_TOKEN_ENV_NAME)
         val pageId = WikiBotUtils.getEnvVariable(PAGE_ID_ENV_NAME)
 
+        val tree = mutableListOf<String>()
+
         NotionPageUtils.execute(notionToken) { client ->
-            parseTree(client, pageId, 0)
+            parseTree(client, tree, pageId, 0)
         }
+
+        val treeToPrint = tree.joinToString("\n")
+        logger.info("tree: \n$treeToPrint")
     }
 
-    private fun parseTree(client: NotionClient, blockId: String, currentLevel: Int) {
+    private fun parseTree(client: NotionClient, tree: MutableList<String>, blockId: String, currentLevel: Int) {
         val separator = LEVEL_SEPARATOR.repeat(currentLevel)
         val separatorNextLevel = LEVEL_SEPARATOR.repeat(currentLevel + 1)
 
@@ -40,29 +45,32 @@ object NotionPageTree : Logging {
             logger.info("")
             logger.info("$separator ${it.type} - ${it.id} - hasChildren: ${it.hasChildren}")
 
+            tree.add("")
+            tree.add("$separator ${it.type}")
+
             if (it.type == BlockType.HeadingOne) {
                 val h1 = it.asHeadingOne()
                 val richTexts = h1.heading1.richText
 
-                printRichTexts(richTexts, separatorNextLevel)
+                printRichTexts(richTexts, separatorNextLevel, tree)
             }
             else if (it.type == BlockType.HeadingTwo) {
                 val h2 = it.asHeadingTwo()
                 val richTexts = h2.heading2.richText
 
-                printRichTexts(richTexts, separatorNextLevel)
+                printRichTexts(richTexts, separatorNextLevel, tree)
             }
             else if (it.type == BlockType.HeadingThree) {
                 val h3 = it.asHeadingThree()
                 val richTexts = h3.heading3.richText
 
-                printRichTexts(richTexts, separatorNextLevel)
+                printRichTexts(richTexts, separatorNextLevel, tree)
             }
             else if (it.type == BlockType.Paragraph) {
                 val p = it.asParagraph()
                 val richTexts = p.paragraph.richText
 
-                printRichTexts(richTexts, separatorNextLevel)
+                printRichTexts(richTexts, separatorNextLevel, tree)
             }
             else if (it.type == BlockType.Callout) {
                 val callout = it.asCallout()
@@ -72,63 +80,80 @@ object NotionPageTree : Logging {
 
                 val richTexts = callout.callout?.richText
 
-                printRichTexts(richTexts, separatorNextLevel)
+                printRichTexts(richTexts, separatorNextLevel, tree)
             }
             else if (it.type == BlockType.BulletedListItem) {
                 val bulletedListItem = it.asBulletedListItem()
 
                 val richTexts = bulletedListItem.bulletedListItem.richText
 
-                printRichTexts(richTexts, separatorNextLevel)
+                printRichTexts(richTexts, separatorNextLevel, tree)
             }
             else if (it.type == BlockType.Toggle) {
                 val toggle = it.asToggle()
 
                 val richTexts = toggle.toggle.richText
 
-                printRichTexts(richTexts, separatorNextLevel)
+                printRichTexts(richTexts, separatorNextLevel, tree)
             }
             else if (it.type == BlockType.ChildPage) {
+                // todo: exclude non-public child pages!!!
+
                 val childPage = it.asChildPage()
 
                 logger.info("$separatorNextLevel child page title: ${childPage.childPage.title}")
+
+                tree.add("$separatorNextLevel ${childPage.childPage.title}")
             }
             else if (it.type == BlockType.Image) {
                 val image = it.asImage()
 
                 logger.info("$separatorNextLevel image type: ${image.image?.type}")
+
+                tree.add("$separatorNextLevel ${image.image?.type}")
             }
 
             // todo: columnList
             // todo: synched blocks :scream:
             // todo: tables
-            // todo: images
             // todo: files?
-
 
             if (
                 it.type != BlockType.ChildPage && // do NOT parse into the child pages
                 it.hasChildren == true
             ) { // block has child blocks -> handle them at the next level
-                parseTree(client, it.id!!, currentLevel + 1)
+                parseTree(client, tree, it.id!!, currentLevel + 1)
             }
         }
     }
 
-    private fun printRichTexts(richTexts: List<PageProperty.RichText>?, separator: String) {
+    private fun printRichTexts(richTexts: List<PageProperty.RichText>?, separator: String, tree: MutableList<String>) {
         richTexts?.forEach {
 
             // mention text (e.g. link on another page has plainText instead of text
-            if (it.plainText?.isNotBlank() == true) {
+            val hasPlainText = it.plainText?.isNotBlank() == true
+            val hasContent = it.text?.content?.isNotBlank() == true
+
+            if (hasPlainText) {
                 logger.info("$separator ${it.plainText}")
+
+                tree.add("$separator ${it.plainText}")
             }
 
             if (it.mention != null) {
                 logger.info("$separator Mention type: ${it.mention?.type}")
+
+                tree.add("$separator Mention type: ${it.mention?.type}")
             }
 
-            if (it.text?.content?.isNotBlank() == true) {
+            if (hasContent) {
                 logger.info("$separator ${it.text}")
+
+                // links have both plainText and text.content -> do not add both
+                // bulleted list items with page links have plain text only
+                if (!hasPlainText) {
+                    tree.add("$separator ${it.text}")
+                }
             }
         }
     }
