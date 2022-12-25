353d36122b5b96b2f5acf14408f4805350f928f4
dmitry-weirdo <dmitry.weirdo@gmail.com>
2022-12-25 02:22:24 +0100

    feat: Add all pages tree to the "All pages tree" page. Takes a lot of time to run.

--- src/main/kotlin/com/dv/telegram/notion/NotionPageIds.kt
+++ src/main/kotlin/com/dv/telegram/notion/NotionPageIds.kt
@@ -4,6 +4,7 @@ object NotionPageIds {
 
     const val MAIN_PAGE = "2b4f00e80cb94440af00e8d83b758f27"
     const val NOTION_API_TEST_PAGE = "24ec680a988441698efe1003a304ded1"
+    const val ALL_PAGES_TREE = "7f50480fb5614e11b9e379b19b311c2a"
 
     const val GERMAN_CITY_CHATS_RU = "9a0effe48cf34cd49c849a9e05c61fb9"
     const val GERMAN_CITY_CHATS_UA = "f1bd74125be747c2ac526fe83515ab41"
