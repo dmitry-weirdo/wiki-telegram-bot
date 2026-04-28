7c709e38d1ddf17e0d0e5167fbb26539f16c92a7
dmitry-weirdo <dmitry.weirdo@gmail.com>
2026-04-28 14:20:01 +0200

    docs: #19 Generate javadocs for the constants of existing pages in `NotionPageIds`. Done with the help of GitHub Copilot - not bad!

--- src/main/kotlin/com/dv/telegram/notion/NotionPageIds.kt
+++ src/main/kotlin/com/dv/telegram/notion/NotionPageIds.kt
@@ -2,13 +2,35 @@ package com.dv.telegram.notion
 
 object NotionPageIds {
 
+    /**
+     * [Помощь украинцам в Германии](https://www.notion.so/russiansabroad/2b4f00e80cb94440af00e8d83b758f27)
+     */
     const val MAIN_PAGE = "2b4f00e80cb94440af00e8d83b758f27"
+
+    /**
+     * [Test page for Notion API](https://www.notion.so/russiansabroad/24ec680a988441698efe1003a304ded1)
+     */
     const val NOTION_API_TEST_PAGE = "24ec680a988441698efe1003a304ded1"
+
+    /**
+     * [All pages tree](https://www.notion.so/russiansabroad/7f50480fb5614e11b9e379b19b311c2a)
+     */
     const val ALL_PAGES_TREE = "7f50480fb5614e11b9e379b19b311c2a"
 
+    /**
+     * [Список чатов и групп по городам и землям Германии](https://www.notion.so/russiansabroad/9a0effe48cf34cd49c849a9e05c61fb9)
+     */
     const val GERMAN_CITY_CHATS_RU = "9a0effe48cf34cd49c849a9e05c61fb9"
+
+    /**
+     * [Список чатів та груп по містам і землям Німеччини](https://www.notion.so/russiansabroad/f1bd74125be747c2ac526fe83515ab41)
+     */
     const val GERMAN_CITY_CHATS_UA = "f1bd74125be747c2ac526fe83515ab41"
+
     const val GERMAN_CITY_CHATS_DE = "" // todo: fill it when the page is added
 
+    /**
+     * [Первые шаги в Германии](https://www.notion.so/russiansabroad/88b6b1a7a2db4043bb0c5a5d10e8ec14)
+     */
     const val FIRST_STEPS_RU = "88b6b1a7a2db4043bb0c5a5d10e8ec14"
 }
